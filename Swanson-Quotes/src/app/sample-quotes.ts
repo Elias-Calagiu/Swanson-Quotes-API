732c0dd53fa7134f0cb7a6da93ca83f288016225
Elias Calagiu <eliasc81@gmail.com>
2022-04-21 12:21:47 -0700

    hardcoded ten swanson quotes as values in quote array

---
+++ Swanson-Quotes/src/app/sample-quotes.ts
@@ -0,0 +1,15 @@
+import { Quote } from './quote';
+
+export const QUOTES: Quote[] = [
+    { id: 1, fullQuote: 'Clear alcohols are for rich women on diets.' },
+    { id: 2, fullQuote: 'Crying: acceptable at funerals and the Grand Canyon.' },
+    { id: 3, fullQuote: 'Under my tutelage, you will grow from boys to men. From men into gladiators. And from gladiators into Swansons.' },
+    { id: 4, fullQuote: 'Never half-ass two things. Whole-ass one thing.' },
+    { id: 5, fullQuote: 'I dont want to paint with a broad brush here, but every single contractor in the world is a miserable, incompetent thief.' },
+    { id: 6, fullQuote: 'Fishing relaxes me. Its like yoga, except I still get to kill something.' },
+    { id: 7, fullQuote: 'No home is complete without a proper toolbox. Heres April and Andys: A hammer, a half eaten pretzel, a baseball card, some cartridge that says Sonic and Hedgehog, a scissor half, a flashlight filled with jellybeans.' },
+    { id: 8, fullQuote: 'Just give me all the bacon and eggs you have. Wait...wait. I worry what you just heard was: Give me a lot of bacon and eggs. What I said was: Give me all the bacon and eggs you have. Do you understand?' },
+    { id: 9, fullQuote: 'When I eat, it is the food that is scared.' },
+    { id: 10, fullQuote: 'Turkey can never beat cow.' }
+
+]
